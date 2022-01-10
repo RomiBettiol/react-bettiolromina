ac4380bbf1ceb0958b038885f18240285c057067
RomiBettiol <romiibettiol@gmail.com>
2022-01-10 14:47:13 -0300

    Primer commit

---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavBar (){
+    return(
+        <>
+        <h1 style = {{color: 'brown'}}>PRIMER DESAFÍO ENTREGALE - NAVBAR</h1>
+            <div>
+                <ul>
+                    <li style={{color: 'red'}}>INICIO</li>
+                    <li style={{color: 'red'}}>NOSOTROS</li>
+                    <li style={{color: 'red'}}>PRODUCTOS</li>
+                    <li style={{color: 'red'}}>MI PERFIL</li>
+                </ul>
+            </div>
+        </>
+    );
+}
